a2a6ce7ae6256f27986932d63e0ae0e3511d0bd8
Shivan <shivanngaur@gmail.com>
2019-12-17 14:19:22 -0500

    Zone Class updated

--- src/Zone.java
+++ src/Zone.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+
 public class Zone {
+    public static final int DEATH = 0;
+    public static final int WALK = 1;
+    private int zoneType;
+    //private int[]zoneRect;
+    private int x,y,width,height;
+    public Zone(int zoneType,int[]rect){
+        this.zoneType=zoneType;
+        rect[0]=x;
+        rect[1]=y;
+        rect[2]=width;
+        rect[3]=height;
+
+    }
 }
