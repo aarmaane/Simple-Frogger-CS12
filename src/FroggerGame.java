7cabcf6c3fdef0eecfc3a22bd7189577ad8b324c
Armaan Randhawa <aarmaane@gmail.com>
2019-12-17 14:06:59 -0500

    Added classes

--- src/FroggerGame.java
+++ src/FroggerGame.java
@@ -1,5 +1,13 @@
-public class FroggerGame{
+import javax.swing.*;
+
+public class FroggerGame extends JFrame {
+    public FroggerGame(){
+        super("Frogger");
+        setSize(500,500);
+        setVisible(true);
+    }
     public static void main(String[] args){
         System.out.println("Epic");
+        FroggerGame game = new FroggerGame();
     }
 }
---
+++ src/Player.java
@@ -0,0 +1,4 @@
+public class Player {
+    private int[] position = new int[2];
+
+}
