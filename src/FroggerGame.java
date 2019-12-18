a3c669598fec7ef88dbea3d97d4dc01195732c6d
Armaan Randhawa <aarmaane@gmail.com>
2019-12-18 14:18:18 -0500

    More window stuff

--- src/FroggerGame.java
+++ src/FroggerGame.java
@@ -1,10 +1,12 @@
 import javax.swing.*;
+import java.awt.event.*;
 
-public class FroggerGame extends JFrame {
+public class FroggerGame extends JFrame implements ActionListener{
+    GamePanel game;
     public FroggerGame(){
         super("Frogger");
-        // Creating the JPanel
-        JPanel pane = new JPanel();
+        // Creating the JPanel with GamePanel class
+        game = new GamePanel(this);
         // Creating the JFrame
         setSize(700,700);
         setResizable(false);
@@ -14,5 +16,25 @@ public class FroggerGame extends JFrame {
     public static void main(String[] args){
         System.out.println("Epic");
         FroggerGame game = new FroggerGame();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        game.repaint();
+    }
+}
+
+class GamePanel extends JPanel implements ActionListener{
+    private boolean[] keys;
+    private FroggerGame gameFrame;
+    public GamePanel(FroggerGame game){
+        keys = new boolean[KeyEvent.KEY_LAST + 1];
+        gameFrame = game;
+        setSize(700,650);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Epic action");
     }
 }
