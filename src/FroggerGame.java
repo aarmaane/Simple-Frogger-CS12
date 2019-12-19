44552c394f55f8c129ac06d58597fa06fc7c2848
Armaan Randhawa <aarmaane@gmail.com>
2019-12-18 22:53:49 -0500

    Random combo of window stuff

--- src/FroggerGame.java
+++ src/FroggerGame.java
@@ -1,40 +1,85 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 
-public class FroggerGame extends JFrame implements ActionListener{
+public class FroggerGame extends JFrame{
     GamePanel game;
+    Timer myTimer;
     public FroggerGame(){
         super("Frogger");
         // Creating the JPanel with GamePanel class
         game = new GamePanel(this);
         // Creating the JFrame
         setSize(700,700);
+        add(game); // Adding the JPanel
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+        // Starting a timer
+        myTimer = new Timer(10, new TickListener());	 // trigger every 100 ms
+        myTimer.start();
+    }
+    // Ticks
+    class TickListener implements ActionListener{
+        public void actionPerformed(ActionEvent evt){
+            if(game!= null && game.ready){
+                game.repaint();
+                System.out.println("sssss");
+            }
+        }
     }
     public static void main(String[] args){
         System.out.println("Epic");
         FroggerGame game = new FroggerGame();
     }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        game.repaint();
-    }
 }
 
-class GamePanel extends JPanel implements ActionListener{
-    private boolean[] keys;
+class GamePanel extends JPanel implements KeyListener{
+    public boolean ready = true;
     private FroggerGame gameFrame;
     public GamePanel(FroggerGame game){
-        keys = new boolean[KeyEvent.KEY_LAST + 1];
         gameFrame = game;
         setSize(700,650);
+        addKeyListener(this);
+    }
+    public void addNotify() {
+        super.addNotify();
+        requestFocus();
+        ready = true;
+    }
+    public void removeNotify(){
+        super.removeNotify();
+        ready = false;
+    }
+    public void paintComponent(Graphics g){
+        System.out.println("Painting");
+        g.setColor(new Color(255, 0, 10));
+        g.fillRect(0,0,getWidth(),getHeight());
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-        System.out.println("Epic action");
+    public void keyTyped(KeyEvent e) {
+
+    }
+    @Override
+    public void keyPressed(KeyEvent e) {
+        System.out.println(e.getKeyCode());
+        if(e.getKeyCode() == KeyEvent.VK_W){
+            System.out.println("up");
+        }
+        if(e.getKeyCode() == KeyEvent.VK_S){
+            System.out.println("down");
+        }
+        if(e.getKeyCode() == KeyEvent.VK_A){
+            System.out.println("left");
+        }
+        if(e.getKeyCode() == KeyEvent.VK_D){
+            System.out.println("right");
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
     }
 }
