ce21a0b80d2c56373b6f6c1b868f5ad431e25ef1
Armaan Randhawa <aarmaane@gmail.com>
2019-12-19 14:19:02 -0500

    Starting Player

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,9 @@
+import java.awt.image.BufferedImage;
+
 public class Player {
     private int[] pos = new int[2];
-
+    private BufferedImage[] sprites = new BufferedImage[];
+    public BufferedImage getCurrentImage(){
+        //NEED TO RETURN SOMETHING HERE
+    }
 }
