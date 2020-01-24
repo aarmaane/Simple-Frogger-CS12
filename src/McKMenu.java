509789afa9a63ae758268b5517c41f707362fecf
Shivan <shivanngaur@gmail.com>
2020-01-23 23:12:57 -0500

    Edited Menu

--- src/McKMenu.java
+++ src/McKMenu.java
@@ -16,20 +16,25 @@ public class McKMenu extends JFrame{
         setSize(680,750);//Sie
 
         ImageIcon backPic = new ImageIcon("Images/Background/Background1.png");
-        ImageIcon logoPic= new ImageIcon("Images/Background/logo.png");
+        ImageIcon logoPic= new ImageIcon("Images/Menu/logo.png");
+        ImageIcon blackStrip = new ImageIcon("Images/Menu/blackStrip.png");
         JLabel back = new JLabel(backPic);
         JLabel logo = new JLabel(logoPic);
+        JLabel strip = new JLabel(blackStrip);
         back.setBounds(0, 0,backPic.getIconWidth(),backPic.getIconHeight());
         logo.setBounds(110,160,logoPic.getIconWidth(),logoPic.getIconHeight());
-        layeredPane.add(back, Integer.valueOf(0));
-        layeredPane.add(logo, Integer.valueOf(1));
+        strip.setBounds(0,710,blackStrip.getIconWidth(),blackStrip.getIconHeight());
         JButton startBtn = new JButton("Play Game");
         startBtn.addActionListener(new ClickStart());
         startBtn.setBounds(160,450,350,70);
+        layeredPane.add(back, Integer.valueOf(0));
+        layeredPane.add(logo, Integer.valueOf(1));
         layeredPane.add(startBtn, Integer.valueOf(2));
-        startBtn.setFocusPainted(false);
-        startBtn.setBorder(null);
+        layeredPane.add(strip, Integer.valueOf(3));
+        //startBtn.setFocusPainted(false);
+        //startBtn.setBorder(null);
         setContentPane(layeredPane);
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
